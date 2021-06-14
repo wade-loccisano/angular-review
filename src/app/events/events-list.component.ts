d86aec86787fd03d65d6310bb929cbb730ea4e9b
Weighed <wade.loccisano@gmail.com>
2021-06-14 19:04:08 -0400

    Create and communicated between angular components

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'events-list',
+    template: `
+        <div>
+            <h1>Upcoming Angular Events</h1>
+            <hr />
+            <div class="well">
+                Hello World
+            </div>
+            <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+            <h3>{{ thumbnail.someProperty }}</h3>
+            <button class="btn btn-primary" (click)="thumbnail.logFoo()">
+                Log me some foo
+            </button>
+        </div>
+    `
+})
+
+export class EventsListComponent {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10: am',
+        price: 599.99,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+}
