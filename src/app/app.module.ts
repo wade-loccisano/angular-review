57cbc2d7f39cdd1225d791c9800de2c677a8d747
Weighed <wade.loccisano@gmail.com>
2021-06-19 09:57:08 -0400

    Mess with services and fail horribly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrService } from './common/toastr.service';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/navbar-component';
 
 @NgModule({
@@ -16,6 +18,7 @@ import { NavBarComponent } from './nav/navbar-component';
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService, ToastrService],
   bootstrap: [
     EventsAppComponent,
   ]
